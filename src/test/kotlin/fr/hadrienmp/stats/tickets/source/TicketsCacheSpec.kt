05286a932f6e7d882ee4d6c1f36dcbde6c580059
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-02-07 15:31:01 +0100

    The cache should call the source when expired

--- src/test/kotlin/fr/hadrienmp/stats/tickets/source/TicketsCacheSpec.kt
+++ src/test/kotlin/fr/hadrienmp/stats/tickets/source/TicketsCacheSpec.kt
@@ -7,7 +7,9 @@ import io.kotlintest.specs.StringSpec
 import org.assertj.core.api.Assertions.assertThat
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.*
+import java.time.Duration
 import java.time.LocalDate.*
+import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
 class TicketsCacheSpec: StringSpec({
@@ -15,7 +17,7 @@ class TicketsCacheSpec: StringSpec({
         val tickets = mock(Tickets::class.java)
         val expected = listOf(Ticket(now(), FEATURE), Ticket(now(), BUG))
         given(tickets.all()).willReturn(expected)
-        val ticketsCache = TicketsCache(tickets)
+        val ticketsCache = TicketsCache(tickets, Duration.ofMillis(100))
 
         val actual = ticketsCache.all()
 
@@ -25,19 +27,39 @@ class TicketsCacheSpec: StringSpec({
     "tickets are cached" {
         val tickets = mock(Tickets::class.java)
         given(tickets.all()).willReturn(listOf(Ticket(now(), FEATURE), Ticket(now(), BUG)))
-        val ticketsCache = TicketsCache(tickets)
+        val ticketsCache = TicketsCache(tickets, Duration.ofMillis(100))
 
         ticketsCache.all()
         ticketsCache.all()
 
         verify(tickets, times(1)).all()
     }
+
+    "tickets are cached 2" {
+        val tickets = mock(Tickets::class.java)
+        given(tickets.all()).willReturn(listOf(Ticket(now(), FEATURE), Ticket(now(), BUG)))
+        val duration = Duration.ofMillis(100)
+        val ticketsCache = TicketsCache(tickets, duration)
+
+        ticketsCache.all()
+        Thread.sleep(duration.toMillis())
+        ticketsCache.all()
+
+        verify(tickets, times(2)).all()
+    }
 })
 
-class TicketsCache(tickets: Tickets):Tickets{
+class TicketsCache(private val tickets: Tickets, private val duration: Duration):Tickets{
     private val list = tickets.all()
+    private val lastUpdate = LocalDateTime.now()
 
-    override fun all() = list
+    override fun all(): List<Ticket> {
+        if (isExpired())
+            tickets.all()
+        return list
+    }
+
+    private fun isExpired() = Duration.between(lastUpdate, LocalDateTime.now()) >= duration
 
     override fun after(date: ZonedDateTime): List<Ticket> {
         TODO("not implemented")
