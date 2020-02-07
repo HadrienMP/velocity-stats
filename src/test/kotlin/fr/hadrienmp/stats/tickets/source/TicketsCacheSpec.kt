7d6bd3f98217bbe08f899ec1d9eb588204fd87e7
Thomas Carpaye <tcarpaye@financeactive.com>
2020-02-07 15:19:00 +0100

    Tickets are cached

--- src/test/kotlin/fr/hadrienmp/stats/tickets/source/TicketsCacheSpec.kt
+++ src/test/kotlin/fr/hadrienmp/stats/tickets/source/TicketsCacheSpec.kt
@@ -8,31 +8,38 @@ import org.assertj.core.api.Assertions.assertThat
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.*
 import java.time.LocalDate.*
+import java.time.ZonedDateTime
 
 class TicketsCacheSpec: StringSpec({
     "Delegates to a tickets source" {
         val tickets = mock(Tickets::class.java)
-        val ticketsCache = TicketsCache(tickets)
         val expected = listOf(Ticket(now(), FEATURE), Ticket(now(), BUG))
         given(tickets.all()).willReturn(expected)
+        val ticketsCache = TicketsCache(tickets)
 
-        val actual = ticketsCache.cachedAll()
+        val actual = ticketsCache.all()
 
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected)
     }
 
-    "test test test" {
+    "tickets are cached" {
         val tickets = mock(Tickets::class.java)
-        val ticketsCache = TicketsCache(tickets)
         given(tickets.all()).willReturn(listOf(Ticket(now(), FEATURE), Ticket(now(), BUG)))
+        val ticketsCache = TicketsCache(tickets)
 
-        ticketsCache.cachedAll()
-        ticketsCache.cachedAll()
+        ticketsCache.all()
+        ticketsCache.all()
 
         verify(tickets, times(1)).all()
     }
 })
 
-class TicketsCache(val tickets: Tickets){
-    fun cachedAll() = tickets.all()
+class TicketsCache(tickets: Tickets):Tickets{
+    private val list = tickets.all()
+
+    override fun all() = list
+
+    override fun after(date: ZonedDateTime): List<Ticket> {
+        TODO("not implemented")
+    }
 }
