ce7923ff0f23cb4df14747fc41fc900376c2d303
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-02-07 15:05:00 +0100

    The cache should not call the source everytime

--- src/test/kotlin/fr/hadrienmp/stats/tickets/source/TicketsCacheSpec.kt
+++ src/test/kotlin/fr/hadrienmp/stats/tickets/source/TicketsCacheSpec.kt
@@ -3,26 +3,36 @@ package fr.hadrienmp.stats.tickets.source
 import fr.hadrienmp.stats.domain.Ticket
 import fr.hadrienmp.stats.domain.TicketType.*
 import fr.hadrienmp.stats.domain.Tickets
+import io.kotlintest.specs.StringSpec
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.*
 import java.time.LocalDate.*
 
-class TicketsCacheSpec {
-
-    @Test
-    internal fun `Delegates to a tickets source`() {
+class TicketsCacheSpec: StringSpec({
+    "Delegates to a tickets source" {
         val tickets = mock(Tickets::class.java)
+        val ticketsCache = TicketsCache(tickets)
         val expected = listOf(Ticket(now(), FEATURE), Ticket(now(), BUG))
         given(tickets.all()).willReturn(expected)
 
-        val actual = TicketsCache(tickets).cachedAll()
+        val actual = ticketsCache.cachedAll()
 
         assertThat(actual).isEqualTo(expected);
     }
 
-    class TicketsCache(val tickets: Tickets){
-        fun cachedAll() = tickets.all()
+    "test test test" {
+        val tickets = mock(Tickets::class.java)
+        val ticketsCache = TicketsCache(tickets)
+        given(tickets.all()).willReturn(listOf(Ticket(now(), FEATURE), Ticket(now(), BUG)))
+
+        ticketsCache.cachedAll()
+        ticketsCache.cachedAll()
+
+        verify(tickets, times(1)).all()
     }
+})
+
+class TicketsCache(val tickets: Tickets){
+    fun cachedAll() = tickets.all()
 }
