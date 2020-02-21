259ff8408f7944605809c05c5d2fcc4b2152a096
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-02-21 10:54:18 +0100

    Create an "after" method that encapsulate the raw response and the parsed version (for debugging purposes)

--- src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
+++ src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
@@ -3,21 +3,13 @@ package fr.hadrienmp.stats.tickets.source.jira
 import com.beust.klaxon.Json
 import com.jcabi.http.Request
 import com.jcabi.http.request.JdkRequest
+import fr.hadrienmp.stats.tickets.source.pivotal.Parser
 import java.time.LocalDate
+import com.jcabi.http.Response as HttpResponse
 
-fun jiraTickets(credentials: Credentials, jiraHost: String, project: String, dateAfter: LocalDate) =
-        JdkRequest("$jiraHost/rest/api/latest/search")
-            .header("authorization", "Basic ${credentials.toBase64()}")
-            .header("content-type", "application/json")
-            .method(Request.POST)
-            .body()
-            .set("""
-                    {
-                        "jql": "project = $project AND created >= $dateAfter ORDER BY created DESC"
-                    }
-                """)
-            .back()
-            .fetch()!!
+class JiraTicketsResponse(val raw: HttpResponse) {
+    val parsed = Parser.parse<Response>(raw.body())
+}
 
 data class Response(val startAt: Int,
                     val maxResults: Int,
@@ -40,3 +32,17 @@ data class Status(val statusCategory: StatusCategory)
 
 data class StatusCategory(val key: String)
 
+fun after(credentials: Credentials, jiraHost: String, project: String, localDate: LocalDate): JiraTicketsResponse {
+    val jql = "project = $project " +
+            "AND created >= ${localDate.minusMonths(6).withDayOfMonth(1)} " +
+            "ORDER BY created DESC"
+    return JiraTicketsResponse(
+            JdkRequest("$jiraHost/rest/api/latest/search")
+                    .header("authorization", "Basic ${credentials.toBase64()}")
+                    .header("content-type", "application/json")
+                    .method(Request.POST)
+                    .body()
+                    .set("""{"jql": "$jql"}""")
+                    .back()
+                    .fetch()!!)
+}
--- src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/JiraManualTest.kt
+++ src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/JiraManualTest.kt
@@ -1,9 +1,6 @@
 package fr.hadrienmp.stats.tickets.source.jira.client
 
-import fr.hadrienmp.stats.tickets.source.jira.Response
-import fr.hadrienmp.stats.tickets.source.jira.credentials
-import fr.hadrienmp.stats.tickets.source.jira.jiraTickets
-import fr.hadrienmp.stats.tickets.source.pivotal.Parser
+import fr.hadrienmp.stats.tickets.source.jira.*
 import java.io.File
 import java.nio.charset.Charset.defaultCharset
 import java.time.LocalDate
@@ -13,11 +10,11 @@ fun main(args: Array<String>) {
     val project = args.first { it.matches(Regex("jira.project=.*")) }.split("=")[1]
     val jiraHost = args.first { it.matches(Regex("jira.host=.*")) }.split("=")[1]
 
-    val responseRaw = jiraTickets(credentials, jiraHost, project, LocalDate.now().minusMonths(6).withDayOfMonth(1))
-    val response = Parser.parse<Response>(responseRaw.body())
+    val jiraTicketsResponse = after(credentials, jiraHost, project, LocalDate.now())
 
-    println(response)
+    println(jiraTicketsResponse.parsed)
 
     File("src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/tickets.json")
-            .writeText(responseRaw.body(), defaultCharset())
+            .writeText(jiraTicketsResponse.raw.body(), defaultCharset())
 }
+
