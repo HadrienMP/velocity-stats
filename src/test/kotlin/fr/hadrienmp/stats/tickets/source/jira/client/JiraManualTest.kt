9e987ff61fb9214a2eb412a70cf854558aac8d82
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-02-21 09:42:13 +0100

    Start deserializing jira tickets

--- src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/JiraManualTest.kt
+++ src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/JiraManualTest.kt
@@ -2,6 +2,8 @@ package fr.hadrienmp.stats.tickets.source.jira.client
 
 import fr.hadrienmp.stats.tickets.source.jira.credentials
 import fr.hadrienmp.stats.tickets.source.jira.jiraTickets
+import fr.hadrienmp.stats.tickets.source.pivotal.Parser
+import fr.hadrienmp.stats.tickets.source.pivotal.model.PivotalTicketsResponse
 import java.io.File
 import java.nio.charset.Charset.defaultCharset
 import java.time.LocalDate
@@ -12,6 +14,22 @@ fun main(args: Array<String>) {
     val jiraHost = args.first { it.matches(Regex("jira.host=.*")) }.split("=")[1]
 
     val jiraTickets = jiraTickets(credentials, jiraHost, project, LocalDate.now().minusMonths(6).withDayOfMonth(1))
+
+    Parser.parse<Response>(jiraTickets.body())!!.let { println(it) }
+
     File("src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/tickets.json")
             .writeText(jiraTickets.body(), defaultCharset())
 }
+
+data class Response(val startAt: Int,
+                    val maxResults: Int,
+                    val total: Int,
+                    val issues: List<Ticket>)
+
+data class Ticket(val fields: Field)
+
+data class Field(val status: Status? = null)
+
+data class Status(val statusCategory: StatusCategory)
+
+data class StatusCategory(val key: String)
