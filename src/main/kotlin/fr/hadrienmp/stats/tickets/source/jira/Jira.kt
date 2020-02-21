3f21e80f39320f34f6d7dd4da6d4efe23b8d9dae
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-02-21 10:43:01 +0100

    Refactor

--- src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
+++ src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
@@ -1,20 +1,42 @@
 package fr.hadrienmp.stats.tickets.source.jira
 
+import com.beust.klaxon.Json
 import com.jcabi.http.Request
 import com.jcabi.http.request.JdkRequest
 import java.time.LocalDate
 
 fun jiraTickets(credentials: Credentials, jiraHost: String, project: String, dateAfter: LocalDate) =
         JdkRequest("$jiraHost/rest/api/latest/search")
-                .header("authorization", "Basic ${credentials.toBase64()}")
-                .header("content-type", "application/json")
-                .method(Request.POST)
-                .body()
-                .set("""
+            .header("authorization", "Basic ${credentials.toBase64()}")
+            .header("content-type", "application/json")
+            .method(Request.POST)
+            .body()
+            .set("""
                     {
                         "jql": "project = $project AND created >= $dateAfter ORDER BY created DESC"
                     }
                 """)
-                .back()
-                .fetch()!!
+            .back()
+            .fetch()!!
+
+data class Response(val startAt: Int,
+                    val maxResults: Int,
+                    val total: Int,
+                    val issues: List<Ticket>)
+
+data class Ticket(val fields: Fields)
+
+data class Fields(val status: Status? = null,
+                  @Json(name = "resolutiondate")
+                  val acceptedAt: String? = null,
+                  @Json(name = "customfield_10002")
+                  val estimate: Float = 0f,
+                  @Json(name = "issuetype")
+                  val type: IssueType)
+
+data class IssueType(val name: String)
+
+data class Status(val statusCategory: StatusCategory)
+
+data class StatusCategory(val key: String)
 
--- src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/JiraManualTest.kt
+++ src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/JiraManualTest.kt
@@ -1,35 +1,23 @@
 package fr.hadrienmp.stats.tickets.source.jira.client
 
+import fr.hadrienmp.stats.tickets.source.jira.Response
 import fr.hadrienmp.stats.tickets.source.jira.credentials
 import fr.hadrienmp.stats.tickets.source.jira.jiraTickets
 import fr.hadrienmp.stats.tickets.source.pivotal.Parser
 import java.io.File
 import java.nio.charset.Charset.defaultCharset
 import java.time.LocalDate
-import java.time.ZonedDateTime
 
 fun main(args: Array<String>) {
     val credentials = credentials(args)
     val project = args.first { it.matches(Regex("jira.project=.*")) }.split("=")[1]
     val jiraHost = args.first { it.matches(Regex("jira.host=.*")) }.split("=")[1]
 
-    val jiraTickets = jiraTickets(credentials, jiraHost, project, LocalDate.now().minusMonths(6).withDayOfMonth(1))
+    val responseRaw = jiraTickets(credentials, jiraHost, project, LocalDate.now().minusMonths(6).withDayOfMonth(1))
+    val response = Parser.parse<Response>(responseRaw.body())
 
-    Parser.parse<Response>(jiraTickets.body())!!.let { println(it) }
+    println(response)
 
     File("src/test/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/tickets.json")
-            .writeText(jiraTickets.body(), defaultCharset())
+            .writeText(responseRaw.body(), defaultCharset())
 }
-
-data class Response(val startAt: Int,
-                    val maxResults: Int,
-                    val total: Int,
-                    val issues: List<Ticket>)
-
-data class Ticket(val fields: Fields)
-
-data class Fields(val status: Status? = null, val resolutiondate: String? = null, val customfield_10002: Float = 0f)
-
-data class Status(val statusCategory: StatusCategory)
-
-data class StatusCategory(val key: String)
