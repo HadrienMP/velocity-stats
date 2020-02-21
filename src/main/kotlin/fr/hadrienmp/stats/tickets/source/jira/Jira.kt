1ef3afd83222adfbc90e14eb0afe9214b374e63f
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-02-21 09:09:27 +0100

    Add a credentials factory

--- src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
+++ src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
@@ -20,10 +20,15 @@ fun jiraTickets(credentials: Credentials, project: String, dateAfter: LocalDate,
                 .back()
                 .fetch()
 
+fun credentials(args: Array<String>): Credentials {
+    val userName = args.first { it.matches(Regex("jira.user=.*")) }.split("=")[1]
+    val password = args.first { it.matches(Regex("jira.password=.*")) }.split("=")[1]
+    return Credentials(userName, password)
+}
+
 class Credentials(private val userName: String, private val password: String) {
     fun toBase64(): String {
         val bytesCredentials = "$userName:$password".toByteArray(Charset.defaultCharset())
         return Base64.getEncoder().encodeToString(bytesCredentials)!!
     }
-
 }
