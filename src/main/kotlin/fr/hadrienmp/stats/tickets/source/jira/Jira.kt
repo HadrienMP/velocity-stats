43bb1f6e196e887d813baf47502510095c21752b
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-02-21 09:14:25 +0100

    Extract Jira Credentials

--- src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
+++ src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/Jira.kt
@@ -2,9 +2,7 @@ package fr.hadrienmp.stats.tickets.source.jira
 
 import com.jcabi.http.Request
 import com.jcabi.http.request.JdkRequest
-import java.nio.charset.Charset
 import java.time.LocalDate
-import java.util.*
 
 fun jiraTickets(credentials: Credentials, jiraHost: String, project: String, dateAfter: LocalDate) =
         JdkRequest("$jiraHost/rest/api/latest/search")
@@ -20,15 +18,3 @@ fun jiraTickets(credentials: Credentials, jiraHost: String, project: String, dat
                 .back()
                 .fetch()
 
-fun credentials(args: Array<String>): Credentials {
-    val userName = args.first { it.matches(Regex("jira.user=.*")) }.split("=")[1]
-    val password = args.first { it.matches(Regex("jira.password=.*")) }.split("=")[1]
-    return Credentials(userName, password)
-}
-
-class Credentials(private val userName: String, private val password: String) {
-    fun toBase64(): String {
-        val bytesCredentials = "$userName:$password".toByteArray(Charset.defaultCharset())
-        return Base64.getEncoder().encodeToString(bytesCredentials)!!
-    }
-}
