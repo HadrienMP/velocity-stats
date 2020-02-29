575ec8d11c332dd48b9069effd9589c8928f2fce
hadrienmp <public@hadrienmp.fr>
2020-02-29 11:32:14 +0200

    Prepare for optional ticket sources

--- src/main/kotlin/fr/hadrienmp/stats/api/web/main.kt
+++ src/main/kotlin/fr/hadrienmp/stats/api/web/main.kt
@@ -3,48 +3,42 @@ package fr.hadrienmp.stats.api.web
 import fr.hadrienmp.lib.web.Port
 import fr.hadrienmp.lib.web.ThymeleafTemplates
 import fr.hadrienmp.lib.web.WebApp
-import fr.hadrienmp.stats.domain.Ticket
-import fr.hadrienmp.stats.domain.countBy
-import fr.hadrienmp.stats.domain.statsOf
-import fr.hadrienmp.stats.domain.timesInDaysByPoint
+import fr.hadrienmp.stats.domain.*
 import fr.hadrienmp.stats.tickets.source.TicketSourceCache
 import fr.hadrienmp.stats.tickets.source.jira.Jira
-import fr.hadrienmp.stats.tickets.source.jira.client.DefaultPageClient
 import fr.hadrienmp.stats.tickets.source.jira.client.jiraPageClientFrom
 import fr.hadrienmp.stats.tickets.source.pivotal.Pivotal
-import fr.hadrienmp.stats.tickets.source.pivotal.client.PivotalClient
 import fr.hadrienmp.stats.tickets.source.pivotal.client.pivotalClientFrom
 import java.time.Duration
 import java.time.ZonedDateTime
 
 fun main(args: Array<String>) {
-    webapp(Port(args), pivotalClientFrom(args), jiraPageClientFrom(args)).start()
+    webapp(Port(args), Pivotal(pivotalClientFrom(args)), Jira(jiraPageClientFrom(args))).start()
 }
 
-fun webapp(port: Port, pivotalClient: PivotalClient, defaultPageClient: DefaultPageClient): WebApp {
-
+fun webapp(port: Port, vararg ticketSources: TicketSource): WebApp {
     ThymeleafTemplates("webapp/").enable()
 
-    return WebApp(port).withRoutes {
-        it.get("/") {
+    return WebApp(port).withRoutes { javalin ->
+        javalin.get("/") {
             it.renderThymeleaf("plots.html")
         }
 
-        val tickets = TicketSourceCache(Duration.ofMinutes(5), Pivotal(pivotalClient), Jira(defaultPageClient))
+        val tickets = TicketSourceCache(Duration.ofMinutes(5), ticketSources)
         val analysisStartDate = ZonedDateTime.now().minusMonths(5).withDayOfMonth(1)
 
 
-        it.get("/stats/tickets-finished-per-month") {
+        javalin.get("/stats/tickets-finished-per-month") {
             it.json(statsOf(tickets.after(analysisStartDate), Ticket::finishMonth))
         }
-        it.get("/stats/tickets-finished-per-week") {
+        javalin.get("/stats/tickets-finished-per-week") {
             it.json(statsOf(tickets.after(analysisStartDate), Ticket::finishWeek))
         }
-        it.get("/stats/cycle-times") {
+        javalin.get("/stats/cycle-times") {
             it.json(tickets.after(analysisStartDate).timesInDaysByPoint(Ticket::cycleTime))
         }
-        it.get("/stats/points-repartition") {
-            it.json(tickets.after(analysisStartDate)
+        javalin.get("/stats/points-repartition") { context ->
+            context.json(tickets.after(analysisStartDate)
                     .countBy(Ticket::points)
                     .mapKeys { "${it.key} points" })
         }
