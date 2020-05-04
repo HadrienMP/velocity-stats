ce48f01bb7f80c9f6c7b8b2fba409087b4cf9976
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-05-04 09:32:58 +0200

    Add logs to debug heroku

--- src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/DefaultPageClient.kt
+++ src/main/kotlin/fr/hadrienmp/stats/tickets/source/jira/client/DefaultPageClient.kt
@@ -2,6 +2,7 @@ package fr.hadrienmp.stats.tickets.source.jira.client
 
 import com.jcabi.http.request.JdkRequest
 import fr.hadrienmp.stats.tickets.source.pivotal.Parser
+import org.slf4j.LoggerFactory
 import java.net.URLEncoder
 import java.time.LocalDate
 
@@ -11,6 +12,9 @@ interface PageClient {
 }
 
 class DefaultPageClient(val credentials: Credentials, private val jiraHost: String, val project: String, private val jqlCustomization: String = "") : PageClient {
+    companion object {
+        val log = LoggerFactory.getLogger(DefaultPageClient::class.java)
+    }
     override fun ticketsAfter(localDate: LocalDate, offset: Int): Response {
         val url = "$jiraHost/rest/api/2/search?startAt=$offset&maxResults=50&jql=${URLEncoder.encode(jql(localDate), Charsets.UTF_8.name())}"
         val jsonResponse = JdkRequest(url)
@@ -18,6 +22,7 @@ class DefaultPageClient(val credentials: Credentials, private val jiraHost: Stri
                 .header("content-type", "application/json")
                 .fetch()
                 .body()
+        log.info(jsonResponse);
         return Parser.parse<Response>(jsonResponse)!!
     }
 
